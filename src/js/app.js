e5f3d5ecdf3a80b275def4362ac4fac5cfb6bf06
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-09-25 12:07:26 +0200

    Add music list

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,42 @@
-console.log(`Witam w projekcjie`);
+// import utils from './utils.js';
+/* global GreenAudioPlayer */
+
+const songs = [{
+  author: `AUTOR JEDEN`,
+  name: `nazwa jeden`,
+  src: `songs/At_The_Top_David_Renda.mp3`,
+  categories: [`jeden`, `dwa`],
+  rank: 1
+},
+{
+  author: `AUTOR DWA`,
+  name: `nazwa dwa`,
+  src: `songs/BASS_TRAP_Steve_Oxen.mp3`,
+  categories: [`jeden`, `piec`],
+  rank: 2
+},
+{
+  author: `AUTOR TRZY`,
+  name: `nazwa trzy`,
+  src: `songs/Silly_Feet_David_Renda.mp3`,
+  categories: [`jeden`, `cztery`],
+  rank: 3
+},
+{
+  author: `AUTOR CZTERY`,
+  name: `nazwa cztery`,
+  src: `songs/Western_Adventures_-_David_Fesliyan.mp3`,
+  categories: [`jeden`, `trzy`],
+  rank: 4
+}];
+
+const wrapper = document.querySelector(`.music-list`);
+const templateHendleBar = Handlebars.compile(wrapper.innerHTML);
+const generateHTML = templateHendleBar({ songs: songs });
+
+document.querySelector(`.music-list`).innerHTML = generateHTML;
+
+GreenAudioPlayer.init({
+  selector: '.song__track-container', // inits Green Audio Player on each audio container that has class "player"
+  stopOthersOnPlay: true
+});
