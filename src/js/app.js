67b824c89f601a901e7b690ec391fc4b34144abc
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-09-26 12:42:16 +0200

    Add menu hover animation. Add small resolution music list style

--- src/js/app.js
+++ src/js/app.js
@@ -5,28 +5,28 @@ const songs = [{
   author: `AUTOR JEDEN`,
   name: `nazwa jeden`,
   src: `songs/At_The_Top_David_Renda.mp3`,
-  categories: [`jeden`, `dwa`],
+  categories: [`jeden`],
   rank: 1
 },
 {
   author: `AUTOR DWA`,
   name: `nazwa dwa`,
   src: `songs/BASS_TRAP_Steve_Oxen.mp3`,
-  categories: [`jeden`, `piec`],
+  categories: [`jeden,`, `piec`],
   rank: 2
 },
 {
   author: `AUTOR TRZY`,
   name: `nazwa trzy`,
   src: `songs/Silly_Feet_David_Renda.mp3`,
-  categories: [`jeden`, `cztery`],
+  categories: [`jeden,`, `cztery`],
   rank: 3
 },
 {
   author: `AUTOR CZTERY`,
   name: `nazwa cztery`,
   src: `songs/Western_Adventures_-_David_Fesliyan.mp3`,
-  categories: [`jeden`, `trzy`],
+  categories: [`jeden,`, `trzy`],
   rank: 4
 }];
 
