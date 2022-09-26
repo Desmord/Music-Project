c62057634f6b3dab7564a1f228cb6ef71e79b9f9
Mikołaj Chojnacki <mikolaj.chojnacki@wp.pl>
2022-09-26 13:24:03 +0200

    Change player primary color to red, Refaactor homepage sass files, Add function createing data object do display

--- src/js/app.js
+++ src/js/app.js
@@ -1,38 +1,32 @@
-// import utils from './utils.js';
+import { createSongsObject } from './utils.js';
 /* global GreenAudioPlayer */
 
 const songs = [{
-  author: `AUTOR JEDEN`,
-  name: `nazwa jeden`,
-  src: `songs/At_The_Top_David_Renda.mp3`,
+  src: `songs/At_The_Top_-_David_Renda.mp3`,
   categories: [`jeden`],
   rank: 1
 },
 {
-  author: `AUTOR DWA`,
-  name: `nazwa dwa`,
-  src: `songs/BASS_TRAP_Steve_Oxen.mp3`,
-  categories: [`jeden,`, `piec`],
+  src: `songs/BASS_TRAP_-_Steve_Oxen.mp3`,
+  categories: [`jeden`, `piec`],
   rank: 2
 },
 {
-  author: `AUTOR TRZY`,
-  name: `nazwa trzy`,
-  src: `songs/Silly_Feet_David_Renda.mp3`,
-  categories: [`jeden,`, `cztery`],
+  src: `songs/Silly_Feet_-_David_Renda.mp3`,
+  categories: [`jeden`, `cztery`],
   rank: 3
 },
 {
-  author: `AUTOR CZTERY`,
-  name: `nazwa cztery`,
   src: `songs/Western_Adventures_-_David_Fesliyan.mp3`,
-  categories: [`jeden,`, `trzy`],
+  categories: [`jeden`, `trzy`],
   rank: 4
 }];
 
+
+
 const wrapper = document.querySelector(`.music-list`);
 const templateHendleBar = Handlebars.compile(wrapper.innerHTML);
-const generateHTML = templateHendleBar({ songs: songs });
+const generateHTML = templateHendleBar({ songs: createSongsObject(songs) });
 
 document.querySelector(`.music-list`).innerHTML = generateHTML;
 
--- src/js/utils.js
+++ src/js/utils.js
@@ -1,10 +1,28 @@
-const utils = {}; // eslint-disable-line no-unused-vars
 
-utils.createDOMFromHTML = function (htmlString) {
-  let div = document.createElement('div');
-  div.innerHTML = htmlString.trim();
-  return div.firstChild;
+/**
+ * 
+ * @param {src,categorie,rank} songs 
+ * @returns array [{name,author,categories,rank,src}]
+ */
+export const createSongsObject = (songs) => {
+  let newSongs = [];
+
+  songs.forEach(song => {
+    const name = song.src.replace(`songs/`, ``).split(`-`)[0].replaceAll(`_`, ` `);
+    const author = song.src.replace(`songs/`, ``).split(`-`)[1].replaceAll(`_`, ` `).replace(`.mp3`, ``).toUpperCase();
+
+    const categories = song.categories.map(categorie => `${categorie},`);
+    categories[categories.length - 1] = song.categories[categories.length - 1];
+
+    newSongs.push({
+      name: name,
+      author: author,
+      categories: categories,
+      rank: song.rank,
+      src: song.src,
+    });
+
+  });
+
+  return newSongs;
 };
-
-
-export default utils;
